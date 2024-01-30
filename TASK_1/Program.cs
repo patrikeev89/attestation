45cfe31589e83c8ac0ddd2b7f863bae164c5339f
PATRIKEEV_89 <patrikeev89@gmail.com>
2024-01-30 23:49:00 +0500

    Add first task

---
+++ TASK_1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbers(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+}
