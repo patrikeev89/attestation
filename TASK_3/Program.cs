29cd03aa25a21770755254d8e5fa929c66362861
PATRIKEEV_89 <patrikeev89@gmail.com>
2024-01-31 00:00:11 +0500

    Add third task

---
+++ TASK_3/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] Array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayRevers(Array, Array.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayRevers(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayRevers(arr, index - 1);
+        }
+    }
+}
